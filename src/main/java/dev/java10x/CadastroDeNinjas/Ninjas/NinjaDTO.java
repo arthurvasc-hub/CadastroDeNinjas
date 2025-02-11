f610622197f9a07094a2d6994a01ba9fde845cda
arthurvasc <arthurvasc2@gmail.com>
2025-02-11 17:08:46 -0300

    chore: includes the DTO version by the entity and manually maps the Entity x DTO paths in NinjaMapper

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,21 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Component
 public class NinjaDTO {
+    private Long id;
+    private String nome;
+    private String email;
+    private String imgUrl;
+    private String rank;
+    private int idade;
+    private MissoesModel missoes;
 }
